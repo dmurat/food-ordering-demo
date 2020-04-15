40a69dc8ec86291a1bb164159f60488ff68dcc3a
Damir Murat <damir.murat.git@gmail.com>
2020-04-15 18:35:02 +0200

    Adding MyHandlerEnhancerDefinition as a Spring bean.

--- src/main/java/io/axoniq/foodordering/FoodOrderingApplication.java
+++ src/main/java/io/axoniq/foodordering/FoodOrderingApplication.java
@@ -1,12 +1,20 @@
 package io.axoniq.foodordering;
 
+import io.axoniq.foodordering.infrastructure.MyHandlerEnhancerDefinition;
+import org.axonframework.messaging.annotation.HandlerEnhancerDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FoodOrderingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodOrderingApplication.class, args);
+    }
+
+    @Bean
+    HandlerEnhancerDefinition myHandlerEnhancerDefinition() {
+        return new MyHandlerEnhancerDefinition();
     }
 }
